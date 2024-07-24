1384b7f3132975fb65d170c87e2002d6b825f0d2
gabriel <gabrielfrhf@gmail.com>
2024-07-24 14:52:36 -0300

    modified lsp configurations, added c and python

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,7 +1,29 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.clangd.setup({})
-    end,
+    {
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lspconfig.clangd.setup({
+               capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
+        end,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+               sources = cmp.config.sources({
+                   { name = 'nvim_lsp' },
+               })
+           })
+       end,
+    },
 }
