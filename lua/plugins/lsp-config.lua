8e724fd37c5f469959ddb275d80e0a659c89363b
gabriel <gabrielfrhf@gmail.com>
2024-07-24 16:54:58 -0300

    Setting mason to config LSPs automatically

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,16 +4,6 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            lspconfig.clangd.setup({
-               capabilities = capabilities,
-            })
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
-        end,
     },
     {
         "hrsh7th/nvim-cmp",
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,20 @@
 return {
-    "williamboman/mason.nvim",
-    config = function()
-        require("mason").setup()
-    end,
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                    })
+                end,
+            })
+        end,
+    },
 }
